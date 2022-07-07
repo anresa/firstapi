70f50a9b8b6805e474a8fcbcd2510d1ac65a5a61
Anna Sandoval <art_90@web.de>
2022-07-07 16:22:17 +0200

    first commit

--- api/users.js
+++ api/users.js
@@ -1,4 +1,9 @@
-const handler = (request, response) => {
-  return response.json({ message: "Ich bin die /users Route" });
+import connectToMongodb from "./_database/connect-to-mongodb";
+import User from "./_database/models/User";
+
+const handler = async (request, response) => {
+  await connectToMongodb();
+  const users = await User.find({});
+  return response.json(users);
 };
 export default handler;
